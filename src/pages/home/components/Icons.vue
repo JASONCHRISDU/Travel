cd83fa949288f5a3b1b5e4e6a94caab57cfe2c94
jcdu <jasonchrisdudu@gmail.com>
2018-04-09 17:03:56 +0800

    add icons again

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,122 @@
+<template>
+	<div class="icons">
+		<swiper>	    
+		    <swiper-slide v-for="(page, index) of pages" :key="index"> 
+				<div class="icon" v-for="item in page" :key="item.id">
+					<div class="icon-img">
+						<img class="icon-img-content" :src="item.imgUrl" />
+					</div>	
+					<p class="icon-desc">{{item.desc}}</p>
+				</div>
+		    </swiper-slide>				    
+		</swiper>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeIcons',
+		data() {
+			return {
+				iconsList: [
+					{
+						id: '001',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '熱門景點'
+					},
+					{
+						id: '002',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '櫻花季'
+					},
+					{
+						id: '003',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '賞雪季'
+					},
+					{
+						id: '004',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '泡溫泉'
+					},
+					{
+						id: '005',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '一日遊'
+					},
+					{
+						id: '006',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '極光震撼'
+					},
+					{
+						id: '007',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '峽谷灣區'
+					},
+					{
+						id: '008',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '富士山'
+					},
+					{
+						id: '009',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+						desc: '開學季'
+					}
+				]
+			}
+		},
+		computed: {
+			pages() {
+				const pages = [];
+				this.iconsList.forEach((item, index) => {
+					const page = Math.floor(index / 8)
+					if(!pages[page]) {
+						pages[page] = []
+					}
+					pages[page].push(item)
+				})
+				return pages;
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	@import '~styles/varibles.styl'
+	@import '~styles/mixins.styl'
+	.icons	
+		overflow: hidden		
+		padding-bottom: 50%
+		height: 0
+		.icon
+			overflow: hidden
+			float: left
+			position: relative
+			padding-bottom: 25%
+			width: 25%
+			height: 0
+			.icon-img
+				box-sizing: border-box
+				position: absolute		
+				top: 0
+				left: 0
+				right: 0
+				bottom: .44rem
+				padding: .1rem
+				.icon-img-content
+					display: block
+					margin: 0 auto
+					height: 100%
+			.icon-desc
+				ellipsis()				
+				position: absolute
+				left: 0
+				right: 0
+				bottom: 0
+				height: .44rem
+				text-align: center
+				line-height: .44rem				
+				color: $darkTextColor
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">	    
+		    <swiper-slide v-for="item in swiperList" :key="item.id">
+		    	<img class="swiper-img" :src="item.imgUrl" alt="" />
+		    </swiper-slide>		    
+		    <div class="swiper-pagination" slot="pagination"></div>	    	    
+		</swiper>
+	</div>
+</template>
+<script>
+	export default {
+		name: 'HomeSwiper',
+		data() {
+			return {
+				swiperOption: {
+					pagination: '.swiper-pagination',
+					loop: true
+				},
+				swiperList: [
+					{
+						id: '001',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1802/51/e78f936a5b404102.jpg_640x200_c14f0b3a.jpg'
+					},
+					{
+						id: '002',
+						imgUrl: '//img1.qunarzz.com/piao/fusion/1803/98/0a2e0bd8eb0c0802.png_640x200_0aae3331.png'
+					}
+				]
+			}
+		}
+
+	}
+</script>
+<style lang="stylus" scoped>
+	.wrapper >>> .swiper-pagination-bullet-active
+		background: #fff
+	.wrapper
+		overflow: hidden
+		padding-bottom: 31.25%
+		width: 100%
+		height: 0
+		background: #eee		
+		.swiper-img
+			width: 100%
+	
+</style>
